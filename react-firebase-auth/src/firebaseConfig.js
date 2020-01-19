1000615ebbc012d28c922888530fbcd690b04f90
Yitendra Kumar <30658445+yitendra@users.noreply.github.com>
2020-01-19 22:18:17 +0530

    Updated variable

--- react-firebase-auth/src/firebaseConfig.js
+++ react-firebase-auth/src/firebaseConfig.js
@@ -1,11 +1,13 @@
+//This is incorrect config variable.
+//You can get your from your Firebase App
 const firebaseConfig = {
-    apiKey: "AIzaSyD-_WeURx8KnVdgpRjXI4UAT42jVg58te4",
-    authDomain: "react-firebase-auth-af5c0.firebaseapp.com",
-    databaseURL: "https://react-firebase-auth-af5c0.firebaseio.com",
-    projectId: "react-firebase-auth-af5c0",
-    storageBucket: "react-firebase-auth-af5c0.appspot.com",
-    messagingSenderId: "829727892996",
-    appId: "1:829727892996:web:6060edc0a304cfefce3751"
+    apiKey: "AIzaSyD-_WeURx8KnVdgpRjXI4UAT42jVg58te5",
+    authDomain: "react-firebase-auth-af5d0.firebaseapp.com",
+    databaseURL: "https://react-firebase-auth-af5d0.firebaseio.com",
+    projectId: "react-firebase-auth-af5d0",
+    storageBucket: "react-firebase-auth-af5d0.appspot.com",
+    messagingSenderId: "829727892976",
+    appId: "1:829727892996:web:6060edc0a304cfefde3751"
   };
 
   export default firebaseConfig;
